1cac20d5c4ee7890a9159e5701a1cc132a36f472
dmdev2020 <dmdev2020@gmail.com>
2021-10-17 20:42:15 +0200

    added files from lesson-3.2

---
+++ src/BooleanExample.java
@@ -0,0 +1,10 @@
+public class BooleanExample {
+
+    public static void main(String[] args) {
+        int value1 = 0;
+        int value2 = 10;
+        boolean test = value1 != 0;
+        boolean test2 = value2 > 0;
+        System.out.println(test || test2 || 25 > 100);
+    }
+}
---
+++ src/Lesson3Example.java
@@ -0,0 +1,21 @@
+public class Lesson3Example {
+
+    public static void main(String[] args) {
+//        int value = 5;
+//        int value2 = 3;
+//        int sum = value % value2;
+//        System.out.println(sum);
+
+//        int value = 5;
+//        value = value + 25;
+//        System.out.println(value);
+
+        int value = 10;
+//        value = value + 1;
+//        value += 1;
+        value++;
+        System.out.println(value);
+        value--;
+        System.out.println(value);
+    }
+}
