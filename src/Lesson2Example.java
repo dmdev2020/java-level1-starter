f94f666abda94e666f9bdc03a98ef6def4c7f291
dmdev2020 <dmdev2020@gmail.com>
2021-10-17 20:39:19 +0200

    added files from lesson-3.1

---
+++ src/Lesson2Example.java
@@ -0,0 +1,25 @@
+public class Lesson2Example {
+
+    public static void main(String[] args) {
+        int temp;
+
+        temp = 567;
+        System.out.println(temp);
+
+        int value = 26;
+        value = temp;
+        System.out.println(value);
+
+        double value2 = 78.123;
+        System.out.println(value2);
+
+        boolean booleanValue = true;
+        System.out.println(booleanValue);
+
+        char testValue = 'a';
+        System.out.println(testValue);
+
+        testValue = 't';
+        System.out.println(testValue);
+    }
+}
