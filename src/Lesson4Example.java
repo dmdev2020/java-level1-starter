6cf9b0af075cf740d89e313eba263e47ea20b723
dmdev2020 <dmdev2020@gmail.com>
2021-10-17 20:44:01 +0200

    added files from lesson-4.1

---
+++ src/Lesson4Example.java
@@ -0,0 +1,18 @@
+public class Lesson4Example {
+
+    public static void main(String[] args) {
+        int value = 10;
+//        boolean test = value > 0;
+        if (value > 0) {
+            System.out.println("Число является положительным");
+        } else if (value < 0) {
+            System.out.println("Число является отрицательным");
+        } else if (value < 1000) {
+            System.out.println("Число является отрицательным");
+        } else if (value < 0) {
+            System.out.println("Число является отрицательным");
+        } else {
+            System.out.println("Число является нулем");
+        }
+    }
+}
